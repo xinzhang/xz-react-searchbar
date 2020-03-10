02590fb1386c3f5ff8734858d2ab8171e9db4d27
Xin Zhang <xinzhang@192-168-1-125.tpgi.com.au>
2020-03-10 22:38:40 +1100

    add styles

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,18 +1,87 @@
 import React from 'react';
 import styled from 'styled-components';
+import screen from 'styles/helpers/media';
+import { ReactComponent as Icon } from './icons/search-icon.svg';
 
-const StyledInput = styled.input`
-  padding: 10px 20px;
-  margin: 20px;
-  border-radius: 1000px;
-  height: 20px;
+const mobileHeight = '55px';
+const desktopHeight = '60px';
+
+const Wrapper = styled.div`
   width: 80vw;
-  align-self: center;
-  font-size: 1.8em;
+  border-radius: 30px;
+  border: solid 1px;
+  padding: 0px 20px;
+  margin: 20px;
+  overflow: hidden;
+  position: relative;
+  transition: border-color ease 0.2s;
+  display: flex;
+  align-items: center;
+  background: ${(props) => props.theme.backgroundPrimary};
+  height: ${mobileHeight};
+  ${screen.md} {
+    height: ${desktopHeight};
+  }
 `;
 
-const SearchBar = () => {
-  return <StyledInput placeholder="enter the title..." />;
-};
+const Slide = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  transition: transform 0.2s ease;
+  transform: translateX(${({ focused }) => (focused ? '-45px' : 0)});
+`;
+
+const StyledInput = styled.input`
+  outline: none;
+  apperance: none;
+  background: none;
+  margin-left: 10px;
+  border: 0;
+  font-size: 18px;
+  width: 100%;
+  color: #fff;
+  &::placeholder {
+    opacity: 0.5;
+  }
+`;
+
+const SearchIcon = styled(Icon)`
+  & path {
+    fill: ${(props) => props.theme.whiteColor};
+  }
+  width: 30px;
+`;
+
+const SEARCH_INPUT_DESCRIPTION = 'Search for an actor or movie';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      focused: false,
+    };
+  }
+
+  render() {
+    const { focused } = this.state;
+    return (
+      <Wrapper>
+        <Slide focused={focused}>
+          <SearchIcon />
+          <StyledInput
+            placeholder={SEARCH_INPUT_DESCRIPTION}
+            onFocus={() =>
+              this.setState({
+                focused: true,
+              })
+            }
+            onBlur={() => this.setState({ focused: false })}
+          />
+        </Slide>
+      </Wrapper>
+    );
+  }
+}
 
 export default SearchBar;
